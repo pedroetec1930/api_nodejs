e4b8d36168e8968da42e8b01b1540c62c73d3203
pedroetec1930 <pedrodesenegouveia@gmail.com>
2025-08-19 11:01:57 -0300

    '19.08'

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ import { PrismaClient } from "@prisma/client"
 
 const app = express()
 const prisma = new PrismaClient()
-app.use(express.json())
 
-// let users = []
+app.use(express.json())
 
 app.get("/users", async (req,res) => {
     let users = await prisma.user.findMany()
@@ -13,39 +12,38 @@ app.get("/users", async (req,res) => {
 })
 
 app.post("/users", async (req,res) => {
-    // users.push(req.body)
     await prisma.user.create({
-        data: {
+        data : {
             name : req.body.name,
-            age: Number(req.body.age),
-            email : req.body.email
+            age : Number(req.body.age),
+            email: req.body.email
         }
     })
     res.status(201)     .send("Usuário criado!")
 })
 
-app.put("/users/:id", async (req,res) => {
-    await prisma.user.update({
-        where: {
-            id : req.params.id
-        },
-        data: {
-            name : req.body.name,
-            age: Number(req.body.age),
-            email : req.body.email
-        }
-    })
-    res.status(200)     .send("Usuário editado!")
-})
+// app.put("/users/:id", async (req,res) => {
+//     await prisma.user.update({
+//         where : {
+//             id : req.params.id
+//         },
+//         data: {
+//             name : req.body.name,
+//             age : Number(req.body.age),
+//             email: req.body.email
+//         }
+//     })
+//     res.status(200)     .send("Usuário editado!")
+// })
 
-app.delete("/users/:id", async (req,res) => {
-    await prisma.user.delete({
-        where: {
-            id : req.params.id
-        }
-    })
-    res.status(200)     .send("Usuário deletado!")
-})
+// app.delete("/users/:id", async (req,res) => {
+//     await prisma.user.delete({
+//         where : {
+//             id : req.params.id
+//         }
+//     })
+//     res.status(200)     .send("Usuário deletado!")
+// })
 
 app.listen(3000, () => {
     console.log("Servidor rodando!")
