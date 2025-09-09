9a987f588382a36243cfc662d0af9bbb7557f062
pedroetec1930 <pedrodesenegouveia@gmail.com>
2025-09-09 10:57:03 -0300

    'integracao'

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { PrismaClient } from "@prisma/client"
+import cors from 'cors'
 
 const app = express()
 const prisma = new PrismaClient()
 
+app.use(cors())
 app.use(express.json())
 
 app.get("/users", async (req,res) => {
